29d681c5c9ec9445e564e0aef1a5b8d9d0053ae9
Thomas Cannon <tcannon00@gmail.com>
2023-01-30 00:37:49 -0500

    Add `Users::PasskeysSessionsController#new_challenge`

--- app/controllers/users/passkey_sessions_controller.rb
+++ app/controllers/users/passkey_sessions_controller.rb
@@ -2,6 +2,17 @@
 
 class Users::PasskeySessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  def new_challenge
+    webauthn_user_id = WebAuthn.generate_user_id
+
+    options = PasskeyAuthenticator.relying_party.options_for_authentication(
+      user_verification: "required"
+    )
+
+    session[passkey_session_challenge_session_key] = options.challenge
+
+    render json: options
+  end
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +35,9 @@ class Users::PasskeySessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  protected
+
+  def passkey_session_challenge_session_key
+    return "#{resource_name}_current_credential_challenge"
+  end
 end
