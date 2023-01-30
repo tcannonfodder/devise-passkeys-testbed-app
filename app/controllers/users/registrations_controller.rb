f62685db546b5a6a490f7073f4d862cb63083c48
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 22:01:51 -0500

    Add `passkey_label` as a permitted parameter for RegistrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:new_challenge, :create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,9 +42,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:passkey_label])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
