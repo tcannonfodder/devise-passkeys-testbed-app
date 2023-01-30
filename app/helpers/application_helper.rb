caeb0233a5ef6839e37d979afb4e03c102847633
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 20:12:44 -0500

    WIP: Scaffolding Devise controllers
    * Also added the missing `new_session_path` helper, as listed in:
    	https://github.com/heartcombo/devise/wiki/OmniAuth:-Overview#using-omniauth-without-other-authentications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def new_session_path(scope)
+    new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/passkey_sessions'
+  }
+
 
   root "root#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
