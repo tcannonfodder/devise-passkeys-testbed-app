510bd051fff2452d036024cc7cd659fce8b40438
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 20:13:57 -0500

    Fix typo in `passkey_authenticatable` Devise add_module definition

--- app/passkey_authenticatable/passkey_authenticatable.rb
+++ app/passkey_authenticatable/passkey_authenticatable.rb
@@ -2,4 +2,4 @@ module PasskeyAuthenticatable
 
 end
 
-Devise.add_module :passkey_authenticatable, model: "passkey_authenticatable/model", strategy: true, controller: :passkey_session, route: { session: [nil, :new, :create, :destroy]}
+Devise.add_module :passkey_authenticatable, model: "passkey_authenticatable/model", strategy: true, controller: :passkey_sessions, route: { session: [nil, :new, :create, :destroy]}
