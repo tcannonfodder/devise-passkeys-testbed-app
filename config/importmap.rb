d64cdcf44400360142b23030297117fbb230bec0
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 21:05:05 -0500

    Update importmap to point to `@github/webauthn-json/browser-ponyfill`

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@github/webauthn-json", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.0/dist/esm/webauthn-json.js"
+pin "@github/webauthn-json/browser-ponyfill", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.0/dist/esm/webauthn-json.browser-ponyfill.js"
