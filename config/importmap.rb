7ea4f20ae3d4514df202caec400133d4d53218ae
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 23:01:52 -0500

    Add `registration_form.js`, defines submit form event, use in form
    * The `submitFormEvent` for the registration form:
    	* Reads the challengeURL from the `data-challenge-url` attribute on
    		the form
    	* Pulls the CSRF token from the document
    	* Uses the form data to submit for a new challenge, passing along the
    		email address so that it can be used as part of the user data when
    		setting up the new `WebAuthn` credential for the challenge
    	* uses the generated challenge to create the new `WebAuthn` publickey
    		credential through the browser
    	* Reads the field name that will hold the credential serialized as
    		JSON, stores the value
    	* Calls `form.submit()` to submit the form as a standard HTML form

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "registration_form", preload: true
 pin "@github/webauthn-json/browser-ponyfill", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.0/dist/esm/webauthn-json.browser-ponyfill.js"
