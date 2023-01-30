315334cb1f405063bce04537228111b286672831
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 20:35:32 -0500

    Clean up PasskeyAuthenticatable prototyping code for Autoloading

--- app/passkey_authenticatable/passkey_authenticatable.rb
+++ app/passkey_authenticatable/passkey_authenticatable.rb
@@ -2,4 +2,4 @@ module PasskeyAuthenticatable
 
 end
 
-Devise.add_module :passkey_authenticatable, model: "passkey_authenticatable/model", strategy: true, controller: :passkey_sessions, route: { session: [nil, :new, :create, :destroy]}
+Devise.add_module :passkey_authenticatable, model: "devise/models/passkey_authenticatable", strategy: true, controller: :passkey_sessions, route: { session: [nil, :new, :create, :destroy]}
