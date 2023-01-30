208a69680d9a025bd44b4d3834cb79ff45eb9a34
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 20:48:03 -0500

    Add Route for `sign_up/new_challenge` for registration flow

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     sessions: 'users/passkey_sessions'
   }
 
+  get 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
+
 
   root "root#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
