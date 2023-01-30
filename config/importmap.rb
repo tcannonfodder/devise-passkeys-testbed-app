ba78133535d9721f3253c104ac8ff69aa773cbfb
Thomas Cannon <tcannon00@gmail.com>
2023-01-29 23:37:27 -0500

    Add `conditionalMediationAvailable` helper method

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,5 @@
 
 pin "application", preload: true
 pin "registration_form", preload: true
+pin "conditional_mediation_available", preload: true
 pin "@github/webauthn-json/browser-ponyfill", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.0/dist/esm/webauthn-json.browser-ponyfill.js"
