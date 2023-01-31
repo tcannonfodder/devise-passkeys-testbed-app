25c6f553ee62f88fd0668f92fcba1008eb194dfa
Thomas Cannon <tcannon00@gmail.com>
2023-01-31 16:17:26 -0500

    Add `passey_reauthentication_handler` JS to get reauthentication token
    * An async method that:
    	* Calls the `data-reauthentication-challenge-url` in the given form
    	* Gets the credential from the browser
    	* Calls `data-reauthentication-token-url` to get the reauthenticaiton
    		token
    	* Stores the token (returned in a JSON object under
    		`reauthentication_token`) in the given
    		`data-reauthentication-token-field-name`
    * Can be called as part of a form's larger submission preflight

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application", preload: true
 pin "registration_form", preload: true
 pin "conditional_mediation_available", preload: true
 pin "session_form", preload: true
+pin "passkey_reauthentication_handler", preload: true
 pin "@github/webauthn-json/browser-ponyfill", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.0/dist/esm/webauthn-json.browser-ponyfill.js"
