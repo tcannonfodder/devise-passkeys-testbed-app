e9d96df898a094f73b1bc7e909cabf59949e66f2
Thomas Cannon <tcannon00@gmail.com>
2023-02-02 18:21:40 -0500

    Port `send_devise_notification` over to `PasskeyAuthenticatable`

--- app/passkey_authenticatable/devise/models/passkey_authenticatable.rb
+++ app/passkey_authenticatable/devise/models/passkey_authenticatable.rb
@@ -64,6 +64,83 @@ module Devise
 
     protected
 
+      def devise_mailer
+        Devise.mailer
+      end
+
+      # This is an internal method called every time Devise needs
+      # to send a notification/mail. This can be overridden if you
+      # need to customize the e-mail delivery logic. For instance,
+      # if you are using a queue to deliver e-mails (active job, delayed
+      # job, sidekiq, resque, etc), you must add the delivery to the queue
+      # just after the transaction was committed. To achieve this,
+      # you can override send_devise_notification to store the
+      # deliveries until the after_commit callback is triggered.
+      #
+      # The following example uses Active Job's `deliver_later` :
+      #
+      #     class User
+      #       devise :database_password_authenticatable, :confirmable
+      #
+      #       after_commit :send_pending_devise_notifications
+      #
+      #       protected
+      #
+      #       def send_devise_notification(notification, *args)
+      #         # If the record is new or changed then delay the
+      #         # delivery until the after_commit callback otherwise
+      #         # send now because after_commit will not be called.
+      #         # For Rails < 6 use `changed?` instead of `saved_changes?`.
+      #         if new_record? || saved_changes?
+      #           pending_devise_notifications << [notification, args]
+      #         else
+      #           render_and_send_devise_message(notification, *args)
+      #         end
+      #       end
+      #
+      #       private
+      #
+      #       def send_pending_devise_notifications
+      #         pending_devise_notifications.each do |notification, args|
+      #           render_and_send_devise_message(notification, *args)
+      #         end
+      #
+      #         # Empty the pending notifications array because the
+      #         # after_commit hook can be called multiple times which
+      #         # could cause multiple emails to be sent.
+      #         pending_devise_notifications.clear
+      #       end
+      #
+      #       def pending_devise_notifications
+      #         @pending_devise_notifications ||= []
+      #       end
+      #
+      #       def render_and_send_devise_message(notification, *args)
+      #         message = devise_mailer.send(notification, self, *args)
+      #
+      #         # Deliver later with Active Job's `deliver_later`
+      #         if message.respond_to?(:deliver_later)
+      #           message.deliver_later
+      #         # Remove once we move to Rails 4.2+ only, as `deliver` is deprecated.
+      #         elsif message.respond_to?(:deliver_now)
+      #           message.deliver_now
+      #         else
+      #           message.deliver
+      #         end
+      #       end
+      #
+      #     end
+      #
+      def send_devise_notification(notification, *args)
+        message = devise_mailer.send(notification, self, *args)
+        # Remove once we move to Rails 4.2+ only.
+        if message.respond_to?(:deliver_now)
+          message.deliver_now
+        else
+          message.deliver
+        end
+      end
+
       if Devise.activerecord51?
         def send_email_changed_notification?
           self.class.send_email_changed_notification && saved_change_to_email? && !@skip_email_changed_notification
