551a149321a2dbb2eb224cec85747066bbc6c59f
Thomas Cannon <tcannon00@gmail.com>
2023-02-03 22:29:59 -0500

    Monkeypatch `Devise.sign_in_after_reset_password`

--- config/initializers/passkey_authenticatable.rb
+++ config/initializers/passkey_authenticatable.rb
@@ -1,2 +1,7 @@
+module Devise
+  mattr_accessor :sign_in_after_emergency_passkey_registration
+  @@sign_in_after_emergency_passkey_registration = true
+end
+
 Devise.add_module :passkey_authenticatable, route: { session: [nil, :new, :create, :destroy]}, model: "devise/models/passkey_authenticatable", strategy: true, controller: :passkey_sessions
 Devise.add_module :passkey_recoverable,  controller: :emergency_passkey_registrations
